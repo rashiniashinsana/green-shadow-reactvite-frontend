2967cdd0f6770abfee773b9c71c511d621e3ff00
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2025-01-17 00:38:50 +0530

    Configure Store

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,13 +1,23 @@
+import { configureStore } from "@reduxjs/toolkit";
 import CropSlice from "../reducers/CropSlice.tsx";
-import {configureStore} from "@reduxjs/toolkit";
-
-
-
+import EquipmentSlice from "../reducers/EquipmentSlice.tsx";
+import FieldSlice from "../reducers/FieldSlice.tsx";
+import StaffSlice from "../reducers/StaffSlice.tsx";
+import VehicleSlice from "../reducers/VehicleSlice.tsx";
 
 const store = configureStore({
     reducer: {
-        crop: CropSlice
-    }
+        field: FieldSlice,
+        crop: CropSlice,
+        equipment: EquipmentSlice,
+        vehicle: VehicleSlice,
+        staff: StaffSlice,
+    },
 });
 
+
 export default store;
+
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
