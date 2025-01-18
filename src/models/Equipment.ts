e4dad9710616ad1d03dcc5e0b649592030c35520
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2025-01-18 21:14:55 +0530

    changed to proper name

--- src/models/Equipment.ts
+++ src/models/Equipment.ts
@@ -1,19 +1,15 @@
 export class Equipment {
-    equipmentId : string;
-    equipmentName: string;
-    equipmentType: string;
-    status: string;
-    fieldCode: string;
-    staffId: string;
+    equipmentId!: string;
+    equipmentName!: string;
+    equipmentType!: string;
+    staffId!: string;
+    fieldId!: string;
 
-    constructor(equipmentId: string, equipmentName: string,equipmentType:string,status: string,fieldCode:string,staffId:string) {
-
-    this.equipmentId = equipmentId;
-    this.equipmentName = equipmentName;
-    this.equipmentType = equipmentType;
-    this.status = status;
-    this.fieldCode = fieldCode;
-    this.staffId = staffId;
-
+    constructor(equipmentId: string, equipmentName: string, equipmentType: string, staffId: string, fieldId: string) {
+        this.equipmentId = equipmentId;
+        this.equipmentName = equipmentName;
+        this.equipmentType = equipmentType;
+        this.staffId = staffId;
+        this.fieldId = fieldId;
     }
 }
