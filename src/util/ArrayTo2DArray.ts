80049e5621114bee1803680c790ff9a87d94a104
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2025-01-19 18:05:20 +0530

    Mange staff vehicle equipment using Arrays

---
+++ src/util/ArrayTo2DArray.ts
@@ -0,0 +1,32 @@
+import {Staff} from "../models/Staff.ts";
+import {Vehicle} from "../models/Vehicle.ts";
+import {Equipment} from "../models/Equipment.ts";
+
+export const convertStaffArrayTo2DArray = (staffArray : Staff[]) => {
+    return staffArray.map((staff) => [
+        staff.staffId ?? "",
+        staff.firstName ?? "",
+        staff.lastName ?? "",
+        staff.gender ?? "",
+        staff.contactNo ?? "",
+    ]);
+}
+
+export const convertVehicleArrayTo2DArray = (vehicleArray : Vehicle[]) => {
+    return vehicleArray.map((vehicle) => [
+        vehicle.vehicleCode ?? "",
+        vehicle.licensePlateNumber ?? "",
+        vehicle.remarks ?? "",
+        vehicle.fuelType ?? "",
+        vehicle.staffId === "" ? "Available" : "Assigned",
+    ])
+}
+
+export const convertEquipmentArrayTo2DArray = (equipmentArray : Equipment[]) => {
+    return equipmentArray.map((equipment) => [
+        equipment.equipmentId ?? "",
+        equipment.equipmentName ?? "",
+        equipment.equipmentType ?? "",
+        equipment.staffId === "" ? "Available" : "Assigned",
+    ])
+}
