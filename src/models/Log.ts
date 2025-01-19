79ed777bc0e5595330fee2e898c0794f6601d851
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2025-01-19 23:14:19 +0530

    add log data check

--- src/models/Log.ts
+++ src/models/Log.ts
@@ -1,16 +1,15 @@
 export class Log {
     logCode: string
-    cropDate: string
-    observedImage: File | null
     logDate: string
+    observedImage: File | null
     logDetail: string
     fieldCodes: string[]
     cropCodes: string[]
     staffIds: string[]
 
-    constructor(logCode: string, cropDate: string, observedImage: File|null = null, logDate: string, logDetail: string, fieldCodes: string[], cropCodes: string[], staffIds: string[]) {
+    constructor(logCode: string, cropDate: string, observedImage: File | null = null, logDate: string, logDetail: string, fieldCodes: string[], cropCodes: string[], staffIds: string[]) {
         this.logCode = logCode
-        this.cropDate = cropDate
+        this.logDate = cropDate
         this.observedImage = observedImage
         this.logDate = logDate
         this.logDetail = logDetail
