a574f43fa23c4166eb64cce436eb70483b72f5c0
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2025-01-19 18:08:47 +0530

    Add crop data to check crud

--- src/reducers/FieldSlice.tsx
+++ src/reducers/FieldSlice.tsx
@@ -1,26 +1,38 @@
-// @ts-ignore
-import { createSlice } from "@reduxjs/toolkit";
+import {Field} from "../models/Field";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
-export const initialState = [];
+const initialState: Field[] = [{
+    fieldCode: "F001",
+    fieldName: "Field 1",
+    fieldSize: "10",
+    fieldImage1: new File([""], "field1.jpg"),
+    fieldImage2: new File([""], "field2.jpg"),
+    location: {
+        longitude: 0,
+        latitude: 0,
+    },
+    assignStaffs: ["S001", "S002"]
+}]
 
 const fieldSlice = createSlice({
-    name: 'field',
+    name: "field",
     initialState,
     reducers: {
-        addField: (state, action) => {
-            state.push(action.payload);
+        saveField: (state, action : PayloadAction<Field>) => {
+            state.push(action.payload)
         },
-        deleteField: (state, action) => {
-            return state.filter(field => field.fieldCode !== action.payload);
+        updateField: (state, action : PayloadAction<Field>) => {
+            return state.map((field: Field) => field.fieldCode === action.payload.fieldCode
+                ? action.payload
+                 : field
+            );
         },
-        updateField: (state, action) => {
-            const index = state.findIndex(field => field.fieldCode === action.payload.fieldCode);
-            if (index !== -1) {
-                state[index] = action.payload;
-            }
-        },
-    },
-});
+        deleteField: (state, action : PayloadAction<string>) => {
+            console.log(action.payload)
+            return state.filter((field: Field) => field.fieldCode !== action.payload)
+        }
+    }
+})
 
-export const { addField, deleteField, updateField } = fieldSlice.actions;
-export default fieldSlice.reducer;
+export const {saveField, updateField, deleteField} = fieldSlice.actions
+export default fieldSlice.reducer
